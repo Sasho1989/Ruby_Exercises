9991c5e3c9ea9a27da2373b8f3b6c5adb523ed2a
Sasho1989 <60719132+Sasho1989@users.noreply.github.com>
2020-02-11 23:34:16 +0100

    Update 3_numbers_and_math_calculator.rb

--- 3_numbers_and_math_calculator.rb
+++ 3_numbers_and_math_calculator.rb
@@ -4,8 +4,8 @@ c = (a + b).to_i #This will make ruby print a integer number
 puts c
 
 
-a = 25.4
-b = 13.5676
+a = 25
+b = 13
 c = (a + b).to_f #This will make ruby print a decimal number, floating point
 puts c
 
